1e4f73e3a5bd436ccb8ded7ab287c579279bd03e
unknown <khalidabdi3101@gmail.com>
2024-10-05 04:58:14 +0300

    Done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+function makeCapital(str) {
+    
+    return str.split(' ')
+              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(' ');
+}
+
+
+console.log(makeCapital("my name is ali")); 
+console.log(makeCapital("fikrcamp Bootcamp")); 
+console.log(makeCapital("i live In Paris")); 
